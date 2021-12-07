2a601830b01dbfc6b52c7d432c65bc1bb931982f
Darek Desu <4459421+darekdesu@users.noreply.github.com>
2021-12-07 22:15:16 +0000

    Added D2 P2 Y21

--- 2021/02.js
+++ 2021/02.js
@@ -20,7 +20,26 @@ const part1 = data => {
 }
 
 const part2 = data => {
-  throw new Error('Imptement me!');
+  const parsedData = parseData(data);
+  
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  parsedData.forEach(command => {
+    const coordinates = command.split(' ');
+    coordinates[1] = Number(coordinates[1])
+
+    if (coordinates[0] === 'forward') {
+      horizontal += coordinates[1]
+      depth += aim * coordinates[1] 
+    }
+    if (coordinates[0] === 'down') { aim += coordinates[1] }
+    if (coordinates[0] === 'up') { aim -= coordinates[1] }
+
+    return;
+  });
+
+  return horizontal * depth;
 }
 
 module.exports = { part1, part2 };
